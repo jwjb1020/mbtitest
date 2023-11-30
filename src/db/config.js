d5be00f5961a826547e61760935db471cd09d942
jwjb1020 <jwjb1020@gmail.com>
2023-11-30 23:36:26 +0900

    mysql2를 이용하여 커넥션 풀 만들고 db에 연결 함수 만듬,.env로 환경변수 설정

---
+++ src/db/config.js
@@ -0,0 +1,29 @@
+import mysql from "mysql2/promise"
+
+     /*직접 연결
+    const connection = await mysql.createConnection({
+        host: 'your_mysql_host',
+        user: 'your_mysql_user',
+        password: 'your_mysql_password',
+        database: 'your_mysql_database',
+    });
+  
+    export default connection;
+    */
+
+    /* 커넥션 풀을 사용해서 연결 
+     장점 
+     1. 여러 연결을 관리하고 재사용 : 성능향상
+     2. 일정한 개수의 연결을 미리 만들어 놓고 필요할때 사용 :리소스 효율
+     3. 동시에 여러 사용자의 요청을 효과적을 처리 : 동시성 관리
+     */
+ const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'test',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+ })
+
+ export default pool;
