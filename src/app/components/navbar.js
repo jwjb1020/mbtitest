90e3d6e21d0b86266167662131f8d1d679de503f
jwjb1020 <jwjb1020@gmail.com>
2023-12-02 18:31:45 +0900

    Design :  css 수정 및  로고 추가
    나중에 더 이쁘게 꾸밀듯
    
    close : #3

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,14 +2,17 @@
 import Link from "next/link";
 
 
+
+
+
 export default function Navbar(){
     return(
-        <nav>
-        <Link href="/">홈</Link>
-        <Link href="/list">문제 풀기</Link>
-        <Link href="/board">자유게시판</Link>
-        <Link href="/announce">공지사항</Link>
-        <Link href="/create">문제등록</Link>
+        <nav className={`glow flex items-center font-nanumPen space-x-16`}>
+        <Link className="text-white  hover:text-gray-300" href="/">홈</Link>
+        <Link className="text-white hover:text-gray-300" href="/list">문제 풀기</Link>
+        <Link className="text-white hover:text-gray-300" href="/board">자유게시판</Link>
+        <Link className="text-white hover:text-gray-300" href="/announce">공지사항</Link>
+        <Link className="text-white hover:text-gray-300" href="/create">문제등록</Link>
         </nav>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Image from 'next/image'
 import Navbar from './components/navbar'
 import './globals.css'
+import SignupButton from './components/signupBotton'
 
 export const metadata = {
   title: 'MbtiTest',
@@ -10,7 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html>
     <body>
-      <Navbar/>
+      <div className='flex px-4 justify-center relative bg-[#408C3F]'>
+        <Image width={70} height={70} alt='logo' src='/logo.png' className='mr-auto'></Image>
+        <Navbar/>
+        <SignupButton/>
+      </div>
       {children}
     </body>
     </html>
