65568b9dc82022cfcaf7ed866f8886a1aa2de1da
jwjb1020 <jwjb1020@gmail.com>
2023-12-03 22:36:02 +0900

    Fix : a 태그로 버튼 수정

--- src/app/components/signupBotton.js
+++ src/app/components/signupBotton.js
@@ -1,5 +1,31 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
 export default function SignupButton(){
+    const router = useRouter();
+    const goSignupPage =()=>{
+        router.push("/signup")
+
+    }
+    const goLoginPage =()=>{
+        router.push("/login")
+    }
     return(
-        <button className="ml-auto">로그인/회원가입</button>
+        <div className="ml-auto flex justify-center items-center">
+        <a 
+            className="mx-1 px-4 py-2  text-white rounded hover:cursor-pointer"
+            onClick={() => goLoginPage()}
+        >
+            로그인
+        </a>
+        <a className="text-white">|</a>
+        <a 
+            className="mx-1 px-4 py-2  text-white rounded hover:cursor-pointer"
+            onClick={() => goSignupPage()}
+        >
+            회원가입
+        </a>
+    </div>
     )
 }
