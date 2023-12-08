674f3e18e96549133fe555f355a4df9a5da336eb
jwjb1020 <jwjb1020@gmail.com>
2023-12-08 15:41:29 +0900

    Feat : title, content  디비 입력
    - 권한 확인 이후 db에 유저 아이디 정보와 문제 내용 입력

--- src/pages/api/create/title.js
+++ src/pages/api/create/title.js
@@ -1,12 +1,27 @@
 import pool from "@/db/config";
+import jwt from "jsonwebtoken";
 
-export default async function handler(req,res){
-   try {
-    if(!req.cookies.jwt){
-        res.status(401).json({message : "Failed", error : "권한이 없습니다."})
+export default async function handler(req, res) {
+    try {
+    
+        if (!req.cookies.jwt) {
+            res.status(401).json({
+                message: "Failed",
+                error: "권한이 없습니다.",
+            });
+        } else {
+            // 헤더 쿠키에서 사용자 정보 디코딩
+            const decodingJwt = jwt.verify(req.cookies.jwt, "test");
+            const user_id = decodingJwt.userID;
+
+            // 문제정보
+            const { title, content, thumbnail } = req.body;
+            console.log(title, content, thumbnail);
+            const connectDB = await pool;
+            await connectDB.query("insert into question (user_id, title, content, thumbnail_url) values (?,?,?,?)",[user_id, title, content, thumbnail]);
+            res.status(200).json({message: "success"})
+        }
+    } catch (error) {
+        res.status(500).json(error)
     }
-    
-   } catch (error) {
-    
-   }
 }
