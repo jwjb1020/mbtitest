e61ac881558c5eaf91c4b5a333336ff08083b433
jwjb1020 <jwjb1020@gmail.com>
2023-12-11 01:03:18 +0900

    Feat : 문제 디테일 저장 api

--- src/pages/api/create/question.js
+++ src/pages/api/create/question.js
@@ -1,8 +1,31 @@
-import pool from "@/db/config"
+import pool from "@/db/config";
 
-export default async function handler(req,res){
-    const questionInfo = req.body;
-    console.log(questionInfo)
-    const connectDB = await pool;
-    await pool.query("insert into question  ")
+export default async function handler(req, res) {
+    if (req.method == "POST") {
+        try {
+            const questionInfo = req.body;
+            console.log(questionInfo);
+            const questionNumber = questionInfo.questionNumber;
+            const question = questionInfo.question;
+            const answer1 = questionInfo.answers[0];
+            const answer2 = questionInfo.answers[1];
+            const user_id = questionInfo.userId;
+            const question_id = questionInfo.questionId;
+            const connectDB = await pool;
+            await connectDB.query(
+                "insert into questionDetail (questionnumber, question, answer1, answer2, user_id, question_id) values (?,?,?,?,?,?)",
+                [
+                    questionNumber,
+                    question,
+                    answer1,
+                    answer2,
+                    user_id,
+                    question_id,
+                ]
+            );
+            res.status(200).json({message : "success"})
+        } catch (error) {
+            res.status(500).json({message : "failed", error : "sever error"})
+        }
+    }
 }
