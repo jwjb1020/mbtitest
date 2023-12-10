ccae242775b88985c8000daf8b8947afaebdb1ba
jwjb1020 <jwjb1020@gmail.com>
2023-12-11 01:06:06 +0900

    Feat : 문제 디테일 보내는 페이지

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,84 +1,80 @@
-"use client"
-import { useEffect, useState } from "react";
-import CreateQuestion from "../components/createQuestion";
+"use client";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 
-export default function Create(){
+export default function Create() {
     const [currentQuestion, setCurrentQuestion] = useState(1);
+    // const [userId, setUserId] = useState();
     const [question, setQuestion] = useState("");
     const [answer1, setAnswer1] = useState("");
     const [answer2, setAnswer2] = useState("");
-
-    //원하는 기능
-    // 1. 문제의 섬네일 넣는 기능
-    // 2. 문제와 답을 넣는 페이지
-    // 3. 답에 대한 mbti 선택지
-    // 4. fetch를 통해 버튼을 누르면 db에 저장되도록
-    // 5. 문제는 총 열두개 -> 페이지안에 단계별로 만들 수 있도록
-    // 6. 마지막으로 제출을 누르면 로딩중 보이고 문제 작성 완료 보이고 홈으로 갈 수 있도록하기
-    
- 
+    const params = useSearchParams();
 
     const nextToCreatQuestion = () => {
-        // 데이터 보내기 전 준비 
+        const userId = params.get("userId");
+        const questionId = params.get("questionId");
+        setCurrentQuestion(currentQuestion + 1);
+        // 데이터 보내기 전 준비
         const questionData = {
-            questionNumber : currentQuestion,
-            question : question,
-            answers : [answer1,answer2],
+            questionNumber: currentQuestion,
+            question: question,
+            answers: [answer1, answer2],
+            userId: userId,
+            questionId: questionId,
         };
 
-        
-        fetch(`/api/create/question`,{
+        fetch(`/api/create/question`, {
             method: "POST",
             headers: {
-                "Content-Type" : "application/json"
+                "Content-Type": "application/json",
             },
-            body : JSON.stringify(questionData),
+            body: JSON.stringify(questionData),
+            credentials: "include",
         })
-        .then((res)=>res.json())
-        .then((data)=>{
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.message == "success") {
+                    alert("저장성공");
+                    // 데이터 초기화
+                    setQuestion("");
+                    setAnswer1("");
+                    setAnswer2("");
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
 
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-          });
+    return (
+        <div>
+            <div>{currentQuestion} 문제</div>
+            <input
+                type="text"
+                name="question"
+                placeholder="문제를 입력해주세요."
+                value={question}
+                required
+                onChange={(e) => setQuestion(e.target.value)}
+            />
+            <input
+                type="text"
+                name="answer1"
+                placeholder="답변 1"
+                value={answer1}
+                required
+                onChange={(e) => setAnswer1(e.target.value)}
+            />
+            <input
+                type="text"
+                name="answer2"
+                placeholder="답변 2"
+                value={answer2}
+                required
+                onChange={(e) => setAnswer2(e.target.value)}
+            />
 
-        setCurrentQuestion(currentQuestion+1)
-        
-        // 데이터 초기화
-        setQuestion("");
-        setAnswer1("");
-        setAnswer2("");
-    }
-
-    return(
-          <div>
-      <div>{currentQuestion} 문제</div>
-      <input
-        type="text"
-        name="question"
-        placeholder="문제를 입력해주세요."
-        value={question}
-        required
-        onChange={(e) => setQuestion(e.target.value)}
-      />
-      <input
-        type="text"
-        name="answer1"
-        placeholder="답변 1"
-        value={answer1}
-        required
-        onChange={(e) => setAnswer1(e.target.value)}
-      />
-      <input
-        type="text"
-        name="answer2"
-        placeholder="답변 2"
-        value={answer2}
-        required
-        onChange={(e) => setAnswer2(e.target.value)}
-      />
-
-      <button onClick={()=>(nextToCreatQuestion())}>다음</button>
-    </div>
-    )
+            <button onClick={() => nextToCreatQuestion()}>다음</button>
+        </div>
+    );
 }
