869e514dedbfd5c90b0af2eef5553a6b2ed2415b
jwjb1020 <jwjb1020@gmail.com>
2023-12-13 20:46:10 +0900

    Feat : 시작 버튼 컴포넌트

---
+++ src/app/components/StartButton.js
@@ -0,0 +1,11 @@
+export default function StartButton(quesitonId) {
+    const moveToQuestion = () => {};
+    return (
+        <button
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+            onClick={() => moveToQuestion}
+        >
+            시작
+        </button>
+    );
+}
