2c893f21db118fa814b8dff5b0df6f689ecd9ebb
jwjb1020 <jwjb1020@gmail.com>
2023-12-17 21:03:43 +0900

    Feat : 쿠키에서 questionId 가져오기 수정

--- src/pages/api/testsheet/create/mbtitype.js
+++ src/pages/api/testsheet/create/mbtitype.js
@@ -5,11 +5,13 @@ export default async function handler(req, res) {
         try {
             //객체로 받기
             const mbtiTypes = JSON.parse(req.body);
+            // question_id 추가
+            mbtiTypes.question_id = req.cookies.questionId;
             const connectDB = await pool;
             await connectDB.query("insert into mbti_type set ?", [mbtiTypes]);
-            res.status(200).JSON({ message: "success" });
+            res.status(200).json({ message: "success" });
         } catch (error) {
-            res.status(500).JSON({ message: "failed", error: "server error" });
+            res.status(400).json({ message: "failed", error: "server error" });
         }
     }
 }
