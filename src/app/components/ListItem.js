8966586e8e5c9ceec9d627c21b8daed7dea1629d
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-12-17 22:26:05 +0900

    feat : 글 삭제 기능 만들기 완료

--- src/app/components/ListItem.js
+++ src/app/components/ListItem.js
@@ -12,15 +12,26 @@ export default function ListItem({ data }) {
                             <h4>{data[i].title}</h4>
                         </Link>
                         <div className="list-button">
-                        <Link href={`/board/edit/${data[i].post_id}`}>수정✏️</Link>
-                        <span onClick={(e) => 
+                        <Link className="text-white bg-blue-700 hover:bg-blue-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 " href={`/board/edit/${data[i].post_id}`}>수정 ✏️</Link>
+                        <span className= "text-white bg-red-600 hover:bg-red-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 " onClick={(e) => 
                             fetch('/api/post/delete', {
                                 method:"DELETE",
                                 body: data[i].post_id
                             })
-                            .then((response) => response.json())
-                            .then((result) => console.log(result))
-                        }>삭제🗑️</span>
+                            .then((response) => {
+                                if(response.status == 200) {                                    
+                                    e.target.closest('.list-item').style.opacity = 0
+                                    setTimeout(() => {
+                                        e.target.closest('.list-item').style.display = 'none'
+                                    }, 1000)
+                                    return response.json();
+                                } 
+                                else if (response.status == 500) {
+                                    return response.json()
+                                }
+                            })
+                            .then((result) => console.log(result.message))
+                        }>삭제 🗑️</span>
                         </div>
                     </div>
                 );
