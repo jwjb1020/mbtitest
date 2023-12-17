25710dc367cab0a9a31c4c59066d06874a6f7a67
jwjb1020 <jwjb1020@gmail.com>
2023-12-17 17:04:09 +0900

    Feat : 로그인 한 유저에 따라 수정, 삭제 버튼 보이는 코드 작성

--- src/app/components/ListCard.js
+++ src/app/components/ListCard.js
@@ -1,8 +1,30 @@
+"use client";
 import Image from "next/image";
 import StartButton from "./StartButton";
+import DeleteButton from "./DeleteButton";
+import EditButton from "./EditButton";
+import { useEffect, useState } from "react";
 
 export default function ListCard({ data }) {
-    console.log(data)
+    //로그인된 회원 정보 맞추기
+    const [loginedUser, setLoginedUser] = useState("");
+    useEffect(() => {
+        fetch("/api/testsheet/show")
+            .then((res) => res.json())
+            .then((result) => {
+                if ((result.message = "success")) {
+                    // 서버측에서 쿠키에 있는 로그인된 유저 정보 가지고 오기
+                    setLoginedUser(result.decodedToken);
+                }
+            });
+    }, []);
+    // 로그인 된 유저 아이디
+    const userId = loginedUser.userID;
+    console.log(userId);
+
+    // 문제를 작성한 유저 아이디
+    const listUserId = data.user_id;
+    console.log(listUserId);
 
     return (
         <div className="max-w-sm bg-slate-400 border rounded-lg shadow">
@@ -25,7 +47,15 @@ export default function ListCard({ data }) {
                 <p className="mb-3 font-normal text-gray-700 ">
                     {data.content}
                 </p>
-                <StartButton quesitonId ={data.quesiton_id}/>
+                <StartButton quesitonId={data.quesiton_id} />
+                {userId == listUserId ? (
+                    <>
+                        <DeleteButton quesitonId={data.quesiton_id} />
+                        <EditButton quesiton_id={data.quesiton_id} />
+                    </>
+                ) : (
+                    <></>
+                )}
             </div>
         </div>
     );
