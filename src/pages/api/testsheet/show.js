cac357a84fd0b8c4cabe94fad367ac9235f6d6d6
jwjb1020 <jwjb1020@gmail.com>
2023-12-17 17:05:34 +0900

    Feat : 데이터 넘길때 토큰에 있는 유저 정보도 전달

--- src/pages/api/testsheet/show.js
+++ src/pages/api/testsheet/show.js
@@ -1,15 +1,17 @@
 import pool from "@/db/config";
-
+import jwt from "jsonwebtoken";
 export default async function handler(req, res) {
     try {
         if (req.method == "GET") {
             try {
+                const cookies = req.cookies.jwt
+                const decodedToken = jwt.verify(cookies, "test")
                 const connectDB = await pool;
                 const [questionList] = await connectDB.query(
                     "select * from question"
                 );
                 const list = questionList;
-                res.status(200).json({ message: "success", list });
+                res.status(200).json({ message: "success", list ,decodedToken });
                 
             } catch (error) {
                 res.status(404).json({message:"failed", error: "쿼리 잘못됨"})
