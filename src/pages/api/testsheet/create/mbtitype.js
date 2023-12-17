083869a92111f60e3ae12bf67c4a4fa6386f6864
jwjb1020 <jwjb1020@gmail.com>
2023-12-17 17:06:12 +0900

    Feat : 메세지 에러코드 작성

--- src/pages/api/testsheet/create/mbtitype.js
+++ src/pages/api/testsheet/create/mbtitype.js
@@ -3,15 +3,13 @@ import pool from "@/db/config";
 export default async function handler(req, res) {
     if (req.method == "POST") {
         try {
+            //객체로 받기
             const mbtiTypes = JSON.parse(req.body);
-
-            
-            
-            // console.log(keys,values);
-             const connectDB = await pool;
-             await connectDB.query("insert into mbti_type set ?",[mbtiTypes])
+            const connectDB = await pool;
+            await connectDB.query("insert into mbti_type set ?", [mbtiTypes]);
+            res.status(200).JSON({ message: "success" });
         } catch (error) {
-            console.log(error)
+            res.status(500).JSON({ message: "failed", error: "server error" });
         }
     }
 }
