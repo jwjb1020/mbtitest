30a95a6171ba56eba6d252ce2ddbb26e8938f477
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-12-25 20:36:04 +0900

    docs : README 작성

--- src/middleware.js
+++ src/middleware.js
@@ -2,14 +2,17 @@ import { getToken } from "next-auth/jwt";
 import jwt from "jsonwebtoken";
 
 export async function middleware(request) {
-    const session = await getToken({ req: request });
-    console.log(session);
+    // const session = await getToken({ req: request });
+    // console.log(session);
 
-    const ck = request.cookies.get("jwt").value;
-    // Next.js의 edge runtime에서 'bcrypto' 모듈이 지원되지 않아서 하기의 코드는 에러가 생김(서버 측에서 유효성 검증(verify) 사용 권장)
-    // const decodedToken = jwt.verify(ck, process.env.SECRET_KEY);
-    // decode()는 유효성 검증 없이 디코딩만 하는 함수
-    const decodedSessionToken = jwt.decode(ck)
+    // console.log(request.cookies != null)
+    if (request.cookies.get("jwt")) {
+        const ck = request.cookies.get("jwt").value;
+    //     // Next.js의 edge runtime에서 'bcrypto' 모듈이 지원되지 않아서 하기의 코드는 에러가 생김(서버 측에서 유효성 검증(verify) 사용 권장)
+    //     // const decodedToken = jwt.verify(ck, process.env.SECRET_KEY);
+    //     // decode()는 유효성 검증 없이 디코딩만 하는 함수
+        const decodedSessionToken = jwt.decode(ck);
 
-    console.log(decodedSessionToken.role);
+        console.log(decodedSessionToken.role);
+    }
 }
