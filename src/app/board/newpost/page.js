40e4abeba97d76c053a74c6f958c7c802585aca6
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-12-26 16:01:53 +0900

    refactor : 게시판 페이지 코드 줄 바꿈

--- src/app/board/newpost/page.js
+++ src/app/board/newpost/page.js
@@ -1,41 +1,51 @@
-'use client'
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Board() {
-
-    let [title, setTitle] = useState('')
-    let [content, setContent] = useState('')
+    let [title, setTitle] = useState("");
+    let [content, setContent] = useState("");
 
     return (
         <div>
             <h4>자유게시판 글 작성</h4>
             <div>
-                <input onChange={(e) => setTitle(e.target.value)} placeholder="글 제목" />
-                <input onChange={(e) => setContent(e.target.value)} placeholder="글 내용" />
-                <button onClick={() => {
-                    fetch('/api/post/new', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type':'appliication/json',
-                        },
-                        body: JSON.stringify({ title: title, content: content })
-                    })
-                    .then((response) => {
-                        window.location.href='/board/list'
-                        response.json()
-                    }
-                    )
-                    .then((result) => {
-                        if(result.success == true) {
-                            console.log(result.message)
-                        } else {
-                            console.log(result.message)
-                        }
-                    })
-
-                }}>게시물 등록</button>
+                <input
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="글 제목"
+                />
+                <input
+                    onChange={(e) => setContent(e.target.value)}
+                    placeholder="글 내용"
+                />
+                <button
+                    onClick={() => {
+                        fetch("/api/post/new", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "appliication/json",
+                            },
+                            body: JSON.stringify({
+                                title: title,
+                                content: content,
+                            }),
+                        })
+                            .then((response) => {
+                                window.location.href = "/board/list";
+                                response.json();
+                            })
+                            .then((result) => {
+                                if (result.success == true) {
+                                    console.log(result.message);
+                                } else {
+                                    console.log(result.message);
+                                }
+                            });
+                    }}
+                >
+                    게시물 등록
+                </button>
             </div>
         </div>
-    )
+    );
 }
