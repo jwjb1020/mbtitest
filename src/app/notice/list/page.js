76fb06c1d4b928df88759dea07a21d1d3bc7c98f
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-12-28 16:41:35 +0900

    feat : 관리자이고 해당 공지사항 작성자만 수정하고 삭제 할 수 있도록 기능 구현

--- src/app/notice/list/page.js
+++ src/app/notice/list/page.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 
 export default function NoticeList() {
     let [data, setData] = useState([]);
-    let [isAdmin, setIsAdmin] = useState([]);
+    let [isCondition, setIsCondition] = useState([]);
 
     useEffect(() => {
         fetch("/api/notice/list")
@@ -15,12 +15,12 @@ export default function NoticeList() {
                 if ((result.success = true)) {
                     setData(result.noticeList);
                     if (result.decodedToken) {
-                        setIsAdmin(result.decodedToken.role);
+                        setIsCondition(result.decodedToken);
                     }
                 }
             });
     }, []);
-
+    
     return (
         <div>
             <div className="notice-header">
@@ -33,19 +33,24 @@ export default function NoticeList() {
                             <Link href={`/notice/detail/${data[i].notice_id}`}>
                                 <h4>{data[i].title}</h4>
                             </Link>
-                            <div className="list-button">
-                                <Link
-                                    className="text-white bg-blue-700 hover:bg-blue-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                                    href={`/notice/admin/edit/${data[i].notice_id}`}
-                                >
-                                    수정 ✏️
-                                </Link>
-                                <DeleteButton
-                                    buttonType={"noticeDelete"}
-                                    data={data}
-                                    i={i}
-                                />
-                            </div>
+                            {isCondition.role === "ROLE_ADMIN" &&
+                            isCondition.userID === data[i].user_id ? (
+                                <div className="list-button">
+                                    <Link
+                                        className="text-white bg-blue-700 hover:bg-blue-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                                        href={`/notice/admin/edit/${data[i].notice_id}`}
+                                    >
+                                        수정 ✏️
+                                    </Link>
+                                    <DeleteButton
+                                        buttonType={"noticeDelete"}
+                                        data={data}
+                                        i={i}
+                                    />
+                                </div>
+                            ) : (
+                                <></>
+                            )}
                         </div>
                     );
                 })}
