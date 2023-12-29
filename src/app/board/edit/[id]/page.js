c795e1108af3cdf3baa6c3f9b130a58931bd53f8
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-12-29 20:49:33 +0900

    refactor : 줄 바꿈

--- src/app/board/edit/[id]/page.js
+++ src/app/board/edit/[id]/page.js
@@ -3,24 +3,54 @@
 import { useEffect, useState } from "react";
 
 export default function Edit(props) {
-    let [ details, setDeatils ] = useState('')
+    let [details, setDeatils] = useState("");
 
     useEffect(() => {
         fetch("/api/post/detail?postId=" + props.params.id)
             .then((response) => response.json())
-            .then((result) => setDeatils(result.detailedPost[0])                
-            );
+            .then((result) => setDeatils(result.detailedPost[0]));
     });
 
     return (
         <div className="edit-page">
             <h4>수정페이지</h4>
             <form action="/api/post/edit" method="POST">
-                제목: <input type="text" name="title" defaultValue={details.title}/>
-                내용: <input type="text" name="content" defaultValue={details.content}/>
-                작성자: <input type="text" name="author" defaultValue={details.author} readOnly/>
-                <input style={{display : 'none'}} name="post_id" defaultValue={details.post_id}/>
-                <button type="submit">수정</button>
+                <div className="form-data">
+                    제목:{" "}
+                    <input
+                        type="text"
+                        name="title"
+                        defaultValue={details.title}
+                    />
+                </div>
+                <div className="form-data">
+                    내용:{" "}
+                    <input
+                        type="text"
+                        name="content"
+                        defaultValue={details.content}
+                    />
+                </div>
+                <div className="form-data">
+                    작성자:{" "}
+                    <input
+                        type="text"
+                        name="author"
+                        defaultValue={details.author}
+                        readOnly
+                    />
+                </div>
+                <input
+                    style={{ display: "none" }}
+                    name="post_id"
+                    defaultValue={details.post_id}
+                />
+                <button
+                    className="text-white bg-green-700 hover:bg-green-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mr-8"
+                    type="submit"
+                >
+                    수정
+                </button>
             </form>
         </div>
     );
