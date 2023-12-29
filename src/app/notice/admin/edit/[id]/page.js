a6d8fb82d783a53a99b744ddf0a6c24e0a5b5f86
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-12-29 20:48:58 +0900

    refactor : UI 배치를 위한 div 추가

--- src/app/notice/admin/edit/[id]/page.js
+++ src/app/notice/admin/edit/[id]/page.js
@@ -15,23 +15,43 @@ export default function Edit(props) {
         <div className="edit-page">
             <h4>공지시항 수정 페이지</h4>
             <form action="/api/notice/edit" method="POST">
-                제목 :{" "}
-                <input type="text" name="title" defaultValue={details.title} />
-                내용 :{" "}
+                <div className="form-data">
+                    제목 :{" "}
+                    <input
+                        type="text"
+                        name="title"
+                        defaultValue={details.title}
+                    />
+                </div>
+                <div className="form-data">
+                    내용 :{" "}
+                    <input
+                        type="text"
+                        name="content"
+                        defaultValue={details.content}
+                    />
+                </div>
+                <div className="form-data">
+                    작성자 :{" "}
+                    <input
+                        type="text"
+                        name="author"
+                        defaultValue={details.author}
+                        readOnly
+                    />
+                </div>
                 <input
-                    type="text"
-                    name="content"
-                    defaultValue={details.content}
+                    style={{ display: "none" }}
+                    name="notice_id"
+                    defaultValue={details.notice_id}
                 />
-                작성자 :{" "}
-                <input
-                    type="text"
-                    name="author"
-                    defaultValue={details.author}
-                    readOnly
-                />
-                <input style={{display: 'none'}} name="notice_id" defaultValue={details.notice_id} />
-                <button type="submit">수정</button>
+                <button
+                    className="text-white bg-green-700 hover:bg-green-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mr-8"
+                    href="/notice/admin/newpost"
+                    type="submit"
+                >
+                    수정
+                </button>
             </form>
         </div>
     );
